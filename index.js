3cd0b7eee1e4f1d2745771738aa7f0e5b436fd4e
brendanjeffreymcmahon <brendanjeffreymcmahon@gmail.com>
2023-08-22 08:32:32 -0400

    add code an stuff

--- index.js
+++ index.js
@@ -1,19 +1,62 @@
-const { Client } = require('pg');
+const IMAP = require('imap');
+const simpleParser = require('mailparser').simpleParser;
+const AWS = require('aws-sdk');
+const secretsManager = new AWS.SecretsManager();
 
-exports.handler = async (event, context) => {
+async function getSecret(secretName) {
   try {
-    // Your code here
-  } catch (error) {
-    console.error(error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'Internal Server Error' }),
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-        'Content-Type': 'application/json'
+      const data = await secretsManager.getSecretValue({ SecretId: secretName }).promise();
+      if (data.SecretString) {
+          return JSON.parse(data.SecretString);
       }
-    };
+      return null;
+  } catch (error) {
+      console.error(`Error retrieving secret: ${secretName}`, error);
+      throw error;
   }
+}
+
+exports.handler = async (event) => {
+    return new Promise((resolve, reject) => {
+        const imap = new IMAP({
+            user: getSecret("zenbox-email"),
+            password: getSecret("zenbox-password"),
+            host: 'imap.gmail.com',
+            port: 993,
+            tls: true,
+            authTimeout: 3000
+        });
+
+        imap.once('ready', function() {
+            imap.openBox('INBOX', true, function(err, box) {
+                if (err) reject(err);
+
+                // Fetch latest 10 emails
+                const fetchEmails = imap.seq.fetch('1:10', {
+                    bodies: ['HEADER', 'TEXT'],
+                    struct: true
+                });
+
+                fetchEmails.on('message', function(msg) {
+                    msg.on('body', function(stream, info) {
+                        simpleParser(stream, (err, mail) => {
+                            if (err) reject(err);
+                            console.log(mail.subject);
+                            console.log(mail.text);
+                        });
+                    });
+                });
+
+                fetchEmails.once('end', function() {
+                    imap.end();
+                });
+            });
+        });
+
+        imap.once('end', function() {
+            resolve("Emails fetched successfully!");
+        });
+
+        imap.connect();
+    });
 };
